fcc01a3417dad098d5ac206e7703fe5d680014cc
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-25 12:08:44 +0300

    2. Build First API (REST Endpoint) using Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const express = require('express');
+
+const apiServer = express();
+
+apiServer.listen(3000, ()=> console.log('API Server Started'));
+
+apiServer.get('/', function (request, response) {
+    response.send('Hi, Fady. This Message from Express');
+});
