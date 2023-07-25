0a0cb23aa2b387459420800f291954c92a70fb94
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-25 14:50:57 +0300

    2. Simulate Data Source to return List (Array) of Product Objects & Return them in Products API Endpoint

--- src/routers/products.js
+++ src/routers/products.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
+const dataSource = require('../dataSource/dataSource');
+
 const routers = express.Router();
 
 routers.get('/', function(request, response){
-    response.send('Products');
+    const products = dataSource.fetchProducts();
+    response.status(200).json(products);
 });
 
 module.exports = routers;
