2254d8b74fa82fca512d9f110522ffe9fe26f7f4
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-27 13:45:18 +0300

    6. Build GET Request to Get all Cart Items

--- src/dataSource/cart.js
+++ src/dataSource/cart.js
@@ -3,6 +3,8 @@ const { v4: uuidV4 } = require("uuid");
 const cart = [];
 
 const cartDS = {
+  fetchCartItems: () => cart,
+
   addItemToCart: (cartItem) => {
     cart.push(cartItem);
     return uuidV4();
--- src/routers/cart.js
+++ src/routers/cart.js
@@ -7,8 +7,12 @@ const routers = express.Router();
 routers.post("/", (request, response) => {
   const cartItem = request.body;
   console.log(cartItem);
-  const itemId = cartDS.addItemToCart();
+  const itemId = cartDS.addItemToCart(cartItem);
   response.status(201).location(`cart/${itemId}`).send();
+});
+
+routers.get("/", (request, response) => {
+  response.status(200).json(cartDS.fetchCartItems());
 });
 
 module.exports = routers;
