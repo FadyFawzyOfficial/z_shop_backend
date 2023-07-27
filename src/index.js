5fbf5b24064effa900b84bc88707bdce363b233f
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-27 12:51:08 +0300

    3. Link the Cart Routers with Starting (index.js) File

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const productsRouters = require('./routers/products');
+const productsRouters = require("./routers/products");
+const cartRouters = require("./routers/cart");
 
 const apiServer = express();
 
-apiServer.listen(3000, ()=> console.log('API Server Started'));
+apiServer.listen(3000, () => console.log("API Server Started"));
 
-apiServer.get('/', function (request, response) {
-    response.send('Hi, Fady. This Message from Express');
+apiServer.get("/", function (request, response) {
+  response.send("Hi, Fady. This Message from Express");
 });
 
-apiServer.use('/products', productsRouters);
+apiServer.use(express.json());
+apiServer.use("/products", productsRouters);
+apiServer.use("/cart", cartRouters);
